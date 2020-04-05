46cd687057b88f302fb8a91c0f53b3a1fd84ef93
KULLEP <malyukow.maksim@mail.ru>
2020-04-05 15:30:42 +0200

    Updates

--- precache-manifest.1166e59b344def354edd8070c207e402.js
+++ precache-manifest.1166e59b344def354edd8070c207e402.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c39f7a594ec6107c46d2fffd97b480b",
+    "revision": "386a11d3dc5e61837e1d65e5ef7dfc52",
     "url": "/vk-api-happy-mini-apps-react/index.html"
   },
   {
-    "revision": "31cefdd5928fc99bfdf6",
+    "revision": "06a4a73304107e15dafa",
     "url": "/vk-api-happy-mini-apps-react/static/css/2.fd4ec06c.chunk.css"
   },
   {
-    "revision": "31cefdd5928fc99bfdf6",
-    "url": "/vk-api-happy-mini-apps-react/static/js/2.6f0ed36a.chunk.js"
+    "revision": "06a4a73304107e15dafa",
+    "url": "/vk-api-happy-mini-apps-react/static/js/2.cbe3bbfb.chunk.js"
   },
   {
     "revision": "84c0fb3b5e9c985532d69c2f9c3eba5b",
-    "url": "/vk-api-happy-mini-apps-react/static/js/2.6f0ed36a.chunk.js.LICENSE.txt"
+    "url": "/vk-api-happy-mini-apps-react/static/js/2.cbe3bbfb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "951dd5e633efa8157406",
-    "url": "/vk-api-happy-mini-apps-react/static/js/main.24b58b46.chunk.js"
+    "revision": "930ce64fbab8ceb54425",
+    "url": "/vk-api-happy-mini-apps-react/static/js/main.bc9ba714.chunk.js"
   },
   {
     "revision": "487104bceb292fc4e006",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk-api-happy-mini-apps-react/precache-manifest.c7826b876c784a8f3404c684866c10be.js"
+  "/vk-api-happy-mini-apps-react/precache-manifest.1166e59b344def354edd8070c207e402.js"
 );
 
 self.addEventListener('message', (event) => {
